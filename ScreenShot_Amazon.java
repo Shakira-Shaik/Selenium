88ba117a824b37113eaa93205afad808e06312ee
Shakira-Shaik <emailtoshakira@gmail.com>
2024-09-04 15:34:37 -0400

    Add files via upload

---
+++ ScreenShot_Amazon.java
@@ -0,0 +1,34 @@
+package seleniumpackage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class ScreenShot_Amazon {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		// TODO Auto-generated method stub
+		ChromeDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();//to maximize the browser
+	    driver.get("https://www.amazon.in");
+	    WebElement E1= driver.findElement(By.id("twotabsearchtextbox"));
+	    E1.sendKeys("watch");
+	    Thread.sleep(3000);
+		List<WebElement>li =driver.findElements(By.xpath("//div[@class='two-pane-results-container']/div/div"));
+		int count=li.size();
+		li.get(count-9).click();
+	     TakesScreenshot T1= driver; //upcasting
+		File source=T1.getScreenshotAs(OutputType.FILE);
+		File Destination=new File("H:\\Users\\shaki\\Desktop\\AB44\\shakira.png");
+		FileHandler.copy(source, Destination); 
+		
+	}
+
+}
